cf7ec55eb18a82060a7eddd1bde58ae79f4f3127
KeunhyoungLee <keunhyoung5@gmail.com>
2019-01-16 15:38:10 +0900

    Create StackQueue_탑.cpp

---
+++ StackQueue_탑.cpp
@@ -0,0 +1,27 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> solution(vector<int> heights) {
+    vector<int> answer;
+    
+    for(int i=heights.size()-1; i>0; i--){
+        int flag=0;//자신보다 높은 수신탑이 없는 경우에 0
+        for(int j=i-1; j>=0; j--){
+            if(heights[i] < heights[j]){
+                answer.push_back(j+1);
+                flag=1;
+                break;
+            }
+        }
+        if( flag==0 )
+            answer.push_back(0);
+    }
+    answer.push_back(0);// 첫 원소 항상 0
+    
+    reverse(answer.begin(), answer.end());
+    
+    return answer;
+}
