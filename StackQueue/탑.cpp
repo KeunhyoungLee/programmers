8bbf2a78ebee171ea237caf55c151680466f3e46
KeunhyoungLee <keunhyoung5@gmail.com>
2019-01-17 18:13:59 +0900

    Update 탑.cpp

--- StackQueue/탑.cpp
+++ StackQueue/탑.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 vector<int> solution(vector<int> heights) {
     vector<int> answer;
-    
+    //거꾸로 탐색
     for(int i=heights.size()-1; i>0; i--){
         int flag=0;//자신보다 높은 수신탑이 없는 경우에 0
         for(int j=i-1; j>=0; j--){
@@ -16,12 +16,11 @@ vector<int> solution(vector<int> heights) {
                 break;
             }
         }
-        if( flag==0 )
+        if( flag==0 )//자신보다 높은 수신탑이 없는 경우에 0
             answer.push_back(0);
     }
     answer.push_back(0);// 첫 원소 항상 0
-    
     reverse(answer.begin(), answer.end());
-    
+    //역순으로
     return answer;
 }
