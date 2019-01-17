47012710fee6772330c69ead882fd90b0c113ec7
KeunhyoungLee <keunhyoung5@gmail.com>
2019-01-17 18:12:57 +0900

    Update 주식가격.cpp

--- StackQueue/주식가격.cpp
+++ StackQueue/주식가격.cpp
@@ -7,14 +7,14 @@ vector<int> solution(vector<int> prices) {
     vector<int> answer;
     
     for(int i=0; i<prices.size(); i++){
-        answer.push_back(0);
+        answer.push_back(0);//초기값 0
         for(int j=i+1; j<prices.size();j++){
             if(prices[i]>prices[j]){
-                answer[i]++;
+                answer[i]++;//비교하는 값보다 크면 초를 증가시키고 break;
                 break;
             }
             else{
-                answer[i]++;
+                answer[i]++;//작으면 초 증가시키고 계속
             }
         }
     }
