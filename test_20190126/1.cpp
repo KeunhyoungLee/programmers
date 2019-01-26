d66c0875dc1d0a286fcce66f73417af37c7b3b01
KeunhyoungLee <keunhyoung5@gmail.com>
2019-01-26 16:27:24 +0900

    Update and rename test/1.cpp to test_20190126/1.cpp

--- test_20190126/1.cpp
+++ test_20190126/1.cpp
@@ -11,18 +11,21 @@ bool solution(vector<int> arrA, vector<int> arrB) {
         answer=false;
         return answer;
     }
+    //갯수 다를때 바로 out
     queue<int> A;
     queue<int> B;
-    
+    //두 배열 큐에 담기
     for(int i=0; i<arrA.size(); i++){
         A.push(arrA[i]);
         B.push(arrB[i]);
     }
+    //옆으로 한 칸씩 옮기면서 같으면 바로 true하고 out
     for(int i=0; i<arrA.size(); i++){
         if(A==B){
             answer=true;
             return answer;    
         }
+        //다르면 앞에꺼 제거하고 뒤로 
         int tmp=A.front();
         A.pop();
         A.push(tmp);
